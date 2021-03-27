b42f7029f727add51d01e734a284486c78f15697
Jack Swayze <talk2dug@gmail.com>
2021-03-27 15:54:05 -0400

    Video streaming

--- app.js
+++ app.js
@@ -27,7 +27,33 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+app.get('/back_yard.m3u8', (req, res) => {
+    if (mp4frag.m3u8) {
+        res.writeHead(200, {'Content-Type': 'application/vnd.apple.mpegurl'});
+        res.end(mp4frag.m3u8);
+    } else {
+        res.sendStatus(503);//todo maybe send 400
+    }
+});
 
+app.get('/init-back_yard.mp4', (req, res) => {
+    if (mp4frag.initialization) {
+        res.writeHead(200, {'Content-Type': 'video/mp4'});
+        res.end(mp4frag.initialization);
+    } else {
+        res.sendStatus(503);
+    }
+});
+
+app.get('/back_yard:id.m4s', (req, res) => {
+    const segment = mp4frag.getSegment(req.params.id);
+    if (segment) {
+        res.writeHead(200, {'Content-Type': 'video/mp4'});
+        res.end(segment);
+    } else {
+        res.sendStatus(503);
+    }
+});
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
