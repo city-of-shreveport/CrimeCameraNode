5e642165a01d25b7986db6f6fa4e06254bbcd520
Alexander Sanchez <alexanderwsanchez1991@gmail.com>
2021-04-28 15:33:45 -0500

    Automate ZeroTier setup.

--- bin/www.js
+++ bin/www.js
@@ -12,15 +12,19 @@ const { execSync } = require('child_process');
 require('dotenv').config();
 
 /**
+ * Get port from environment and store in Express.
+ */
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
  * Bootstrap and start app.
  */
 bootstrapApp().then(() => {
-  app.set('port', normalizePort(process.env.PORT || '3000'));
-
   var server = http
     .createServer({}, app)
-    .listen(3000, function () {
-      console.log('App listening on port 3000!');
+    .listen(port, function () {
+      console.log(`App listening on port ${port}!`);
     })
     .on('error', onError);
 
@@ -34,24 +38,39 @@ async function bootstrapApp() {
       `${process.env.CAMERA_SERVER}/api/get-config?token=${process.env.API_KEY}&camera=${process.env.CAMERA_IDENTIFIER}`
     );
 
-    var config = JSON.parse(response.body);
+    try {
+      var config = JSON.parse(response.body).cameraConfiguration;
+    } catch (error) {
+      console.log('Unable to parse configuration information. Please ensure that config is valid JSON.');
+    }
+
+    console.log('Optionally setting up and joining ZeroTier network...');
+    try {
+      if (config.zeroTierNetworkID) {
+        execSync(
+          `curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import --quiet;
+          if z=$(curl -s 'https://install.zerotier.com/' | gpg --quiet); then echo "$z" | sudo bash; fi;
+          sudo chmod 755 -R /var/lib/zerotier-one;
+          sudo service zerotier-one restart;
+          sudo zerotier-cli join ${config.zeroTierNetworkID};`
+        );
+      }
+    } catch (error) {}
 
     console.log('Idempotently setting up encryption on video storage device...');
     var driveIsEncrypted = false;
 
     // Check lsblk to see if the virtual partition exists.
     try {
-      driveIsEncrypted = execSync(
-        `lsblk -o NAME,TYPE,SIZE,MODEL | grep ${config.cameraConfiguration.videoDriveEncryptionKey}`
-      )
+      driveIsEncrypted = execSync(`lsblk -o NAME,TYPE,SIZE,MODEL | grep ${config.videoDriveEncryptionKey}`)
         .toString()
-        .includes(config.cameraConfiguration.videoDriveEncryptionKey);
+        .includes(config.videoDriveEncryptionKey);
     } catch (error) {}
 
     // If the lsblk virtual partition does not exist, check blkid to see if the drive is formatted correctly.
     try {
       if (!driveIsEncrypted) {
-        driveIsEncrypted = execSync(`blkid ${config.cameraConfiguration.videoDrivePath} | grep crypto_LUKS`)
+        driveIsEncrypted = execSync(`blkid ${config.videoDrivePath} | grep crypto_LUKS`)
           .toString()
           .includes('crypto_LUKS');
       }
@@ -61,11 +80,9 @@ async function bootstrapApp() {
     try {
       if (driveIsEncrypted) {
         execSync(
-          `
-            sudo mkdir -p ${config.cameraConfiguration.videoDriveMountPath};
-            echo '${config.cameraConfiguration.videoDriveEncryptionKey}' | sudo cryptsetup --batch-mode -d - luksOpen ${config.cameraConfiguration.videoDrivePath} ${config.cameraConfiguration.videoDriveEncryptionKey};
-            sudo mount /dev/mapper/${config.cameraConfiguration.videoDriveEncryptionKey} ${config.cameraConfiguration.videoDriveMountPath};
-          `
+          `sudo mkdir -p ${config.videoDriveMountPath};
+          echo '${config.videoDriveEncryptionKey}' | sudo cryptsetup --batch-mode -d - luksOpen ${config.videoDrivePath} ${config.videoDriveEncryptionKey};
+          sudo mount /dev/mapper/${config.videoDriveEncryptionKey} ${config.videoDriveMountPath}`
         );
       }
     } catch (error) {}
@@ -75,25 +92,12 @@ async function bootstrapApp() {
     } else {
       // Setup and mount encrypted virtual partition.
       execSync(
-        `
-          echo '${config.cameraConfiguration.videoDriveEncryptionKey}' | sudo cryptsetup --batch-mode -d - luksFormat ${config.cameraConfiguration.videoDrivePath};
-          echo '${config.cameraConfiguration.videoDriveEncryptionKey}' | sudo cryptsetup --batch-mode -d - luksOpen ${config.cameraConfiguration.videoDrivePath} ${config.cameraConfiguration.videoDriveEncryptionKey};
-          yes | sudo mkfs.ext4 -q /dev/mapper/${config.cameraConfiguration.videoDriveEncryptionKey};
-          sudo mkdir -p ${config.cameraConfiguration.videoDriveMountPath};
-          sudo mount /dev/mapper/${config.cameraConfiguration.videoDriveEncryptionKey} ${config.cameraConfiguration.videoDriveMountPath};
-          sudo chmod 755 -R ${config.cameraConfiguration.videoDriveMountPath};
-        `,
-        (error, stdout, stderr) => {
-          if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-          }
-          if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-          }
-          console.log(`stdout: ${stdout}`);
-        }
+        `echo '${config.videoDriveEncryptionKey}' | sudo cryptsetup --batch-mode -d - luksFormat ${config.videoDrivePath};
+        echo '${config.videoDriveEncryptionKey}' | sudo cryptsetup --batch-mode -d - luksOpen ${config.videoDrivePath} ${config.videoDriveEncryptionKey};
+        yes | sudo mkfs.ext4 -q /dev/mapper/${config.videoDriveEncryptionKey};
+        sudo mkdir -p ${config.videoDriveMountPath};
+        sudo mount /dev/mapper/${config.videoDriveEncryptionKey} ${config.videoDriveMountPath};
+        sudo chmod 755 -R ${config.videoDriveMountPath}`
       );
     }
   } catch (error) {
