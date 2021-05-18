dd9808b34e082014193424577731136046c4054f
Alexander Sanchez <alexanderwsanchez1991@gmail.com>
2021-05-17 19:49:37 -0500

    Fix execSync issues.

--- bin/www.js
+++ bin/www.js
@@ -5,10 +5,8 @@ const app = require('../app');
 const debug = require('debug')('CrimeCameraNode:server');
 const http = require('http');
 const fs = require('fs');
-const socketApi = require('../socketApi');
-const io = socketApi.io;
 const got = require('got');
-const { execSync } = require('child_process');
+const { exec } = require('child_process');
 require('dotenv').config();
 const dedent = require('dedent-js');
 
@@ -22,6 +20,9 @@ app.set('port', port);
  * Bootstrap and start app.
  */
 bootstrapApp().then(() => {
+  const socketApi = require('../socketApi');
+  const io = socketApi.io;
+
   var server = http
     .createServer({}, app)
     .listen(port, function () {
@@ -41,58 +42,52 @@ async function bootstrapApp() {
 
     var config = JSON.parse(response.body).cameraConfiguration;
 
-    try {
-      if (config.zeroTierNetworkID) {
-        console.log('Joining ZeroTier network...');
-        execSync(dedent`
-          curl -s https://install.zerotier.com | sudo bash;
-          sudo zerotier-cli join ${config.zeroTierNetworkID};
-          sudo chmod 755 -R /var/lib/zerotier-one;
-          sudo service zerotier-one restart;
-        `);
-      }
-    } catch (error) {}
-
-    try {
-      execSync(dedent`
-          sudo iptables -P INPUT ACCEPT &&
-          sudo iptables -P FORWARD ACCEPT &&
-          sudo iptables -P OUTPUT ACCEPT  &&
-          sudo iptables -t nat -F &&
-          sudo iptables -t mangle -F &&
-          sudo iptables -t raw -F &&
-          sudo iptables -F &&
-          sudo iptables -X &&
-          sudo sysctl net.ipv4.conf.eth1.forwarding=1 &&
-          sudo sysctl net.ipv4.conf.wlan0.forwarding=1 &&
-          sudo sysctl net.ipv4.conf.ztuga7sx7i.forwarding=1 &&
-          sudo iptables -t nat -A PREROUTING -p tcp -s 0/0 -d ${config.zeroTierIP} --dport 554 -j DNAT --to 10.10.5.2:554 &&
-          sudo iptables -A FORWARD -p tcp -d ${config.zeroTierIP} --dport 554 -j ACCEPT  &&
-          sudo iptables -t nat -A PREROUTING -p tcp -s 0/0 -d ${config.zeroTierIP} --dport 81 -j DNAT --to 10.10.5.2:80 &&
-          sudo iptables -A FORWARD -p tcp -d ${config.zeroTierIP} --dport 80 -j ACCEPT  &&
-          sudo iptables -t nat -A POSTROUTING -j MASQUERADE &&
-          sudo iptables -t nat -A PREROUTING -p tcp -s 0/0 -d ${config.zeroTierIP} --dport 555 -j DNAT --to 10.10.5.3:554 &&
-          sudo iptables -A FORWARD -p tcp -d ${config.zeroTierIP} --dport 555 -j ACCEPT  &&
-          sudo iptables -t nat -A PREROUTING -p tcp -s 0/0 -d ${config.zeroTierIP} --dport 82 -j DNAT --to 10.10.5.3:80 &&
-          sudo iptables -A FORWARD -p tcp -d ${config.zeroTierIP} --dport 81 -j ACCEPT  &&
-          sudo iptables -t nat -A POSTROUTING -j MASQUERADE &&
-          sudo iptables -t nat -A PREROUTING -p tcp -s 0/0 -d ${config.zeroTierIP} --dport 556 -j DNAT --to 10.10.5.4:554 &&
-          sudo iptables -A FORWARD -p tcp -d ${config.zeroTierIP} --dport 556 -j ACCEPT  &&
-          sudo iptables -t nat -A PREROUTING -p tcp -s 0/0 -d ${config.zeroTierIP} --dport 83 -j DNAT --to 10.10.5.4:80 &&
-          sudo iptables -A FORWARD -p tcp -d ${config.zeroTierIP} --dport 82 -j ACCEPT  &&
-          sudo iptables -t nat -A POSTROUTING -j MASQUERADE
+    if (config.zeroTierNetworkID) {
+      console.log('Joining ZeroTier network...');
+      await execCommand(dedent`
+        curl -s https://install.zerotier.com | sudo bash;
+        sudo zerotier-cli join ${config.zeroTierNetworkID};
+        sudo chmod 755 -R /var/lib/zerotier-one;
+        sudo service zerotier-one restart;
       `);
-    } catch (error) {}
+    }
+
+    await execCommand(dedent`
+      sudo iptables -P INPUT ACCEPT &&
+      sudo iptables -P FORWARD ACCEPT &&
+      sudo iptables -P OUTPUT ACCEPT  &&
+      sudo iptables -t nat -F &&
+      sudo iptables -t mangle -F &&
+      sudo iptables -t raw -F &&
+      sudo iptables -F &&
+      sudo iptables -X &&
+      sudo sysctl net.ipv4.conf.eth1.forwarding=1 &&
+      sudo sysctl net.ipv4.conf.wlan0.forwarding=1 &&
+      sudo sysctl net.ipv4.conf.ztuga7sx7i.forwarding=1 &&
+      sudo iptables -t nat -A PREROUTING -p tcp -s 0/0 -d ${config.zeroTierIP} --dport 554 -j DNAT --to 10.10.5.2:554 &&
+      sudo iptables -A FORWARD -p tcp -d ${config.zeroTierIP} --dport 554 -j ACCEPT  &&
+      sudo iptables -t nat -A PREROUTING -p tcp -s 0/0 -d ${config.zeroTierIP} --dport 81 -j DNAT --to 10.10.5.2:80 &&
+      sudo iptables -A FORWARD -p tcp -d ${config.zeroTierIP} --dport 80 -j ACCEPT  &&
+      sudo iptables -t nat -A POSTROUTING -j MASQUERADE &&
+      sudo iptables -t nat -A PREROUTING -p tcp -s 0/0 -d ${config.zeroTierIP} --dport 555 -j DNAT --to 10.10.5.3:554 &&
+      sudo iptables -A FORWARD -p tcp -d ${config.zeroTierIP} --dport 555 -j ACCEPT  &&
+      sudo iptables -t nat -A PREROUTING -p tcp -s 0/0 -d ${config.zeroTierIP} --dport 82 -j DNAT --to 10.10.5.3:80 &&
+      sudo iptables -A FORWARD -p tcp -d ${config.zeroTierIP} --dport 81 -j ACCEPT  &&
+      sudo iptables -t nat -A POSTROUTING -j MASQUERADE &&
+      sudo iptables -t nat -A PREROUTING -p tcp -s 0/0 -d ${config.zeroTierIP} --dport 556 -j DNAT --to 10.10.5.4:554 &&
+      sudo iptables -A FORWARD -p tcp -d ${config.zeroTierIP} --dport 556 -j ACCEPT  &&
+      sudo iptables -t nat -A PREROUTING -p tcp -s 0/0 -d ${config.zeroTierIP} --dport 83 -j DNAT --to 10.10.5.4:80 &&
+      sudo iptables -A FORWARD -p tcp -d ${config.zeroTierIP} --dport 82 -j ACCEPT  &&
+      sudo iptables -t nat -A POSTROUTING -j MASQUERADE
+    `);
 
     console.log('Idempotently setting up encryption on video storage devices...');
-    setupStorageDrive(config.videoDriveDevicePath, config.videoDriveMountPath, config.videoDriveEncryptionKey);
-    setupStorageDrive(config.buddyDriveDevicePath, config.buddyDriveMountPath, config.buddyDriveEncryptionKey);
+    await setupStorageDrive(config.videoDriveDevicePath, config.videoDriveMountPath, config.videoDriveEncryptionKey);
+    await setupStorageDrive(config.buddyDriveDevicePath, config.buddyDriveMountPath, config.buddyDriveEncryptionKey);
 
-    try {
-      execSync(`sudo mkdir -p /home/pi/videos/cam1;`);
-      execSync(`sudo mkdir -p /home/pi/videos/cam2;`);
-      execSync(`sudo mkdir -p /home/pi/videos/cam3;`);
-    } catch (error) {}
+    await execCommand(`sudo mkdir -p /home/pi/videos/cam1;`);
+    await execCommand(`sudo mkdir -p /home/pi/videos/cam2;`);
+    await execCommand(`sudo mkdir -p /home/pi/videos/cam3;`);
   } catch (error) {
     console.log('Failed to get configuration information from remote server.');
     console.log(error);
@@ -152,64 +147,55 @@ function onListening() {
 /**
  * Helper functions.
  */
-function setupStorageDrive(devicePath, mountPath, encryptionKey) {
+async function setupStorageDrive(devicePath, mountPath, encryptionKey) {
   var driveIsEncrypted = false;
   var driveIsFormatted = false;
 
   // Check lsblk to see if the virtual partition exists.
-  try {
-    driveIsEncrypted = execSync(dedent`lsblk -o NAME,TYPE,SIZE,MODEL | grep ${encryptionKey}`)
-      .toString()
-      .includes(encryptionKey);
-  } catch (error) {}
+  driveIsEncrypted = await execCommand(dedent`lsblk -o NAME,TYPE,SIZE,MODEL | grep ${encryptionKey}`)
+    .toString()
+    .includes(encryptionKey);
 
   // If the lsblk virtual partition does not exist, check blkid to see if the drive is formatted correctly.
-  try {
-    if (!driveIsEncrypted) {
-      driveIsFormatted = execSync(`blkid ${devicePath} | grep crypto_LUKS`).toString().includes('crypto_LUKS');
-
-      if (!driveIsFormatted) {
-        console.log('Formatting drive...');
-        execSync(dedent`echo '${encryptionKey}' | sudo cryptsetup --batch-mode -d - luksFormat ${devicePath};`);
-      }
+  if (!driveIsEncrypted) {
+    driveIsFormatted = await execCommand(`blkid ${devicePath} | grep crypto_LUKS`).toString().includes('crypto_LUKS');
+    if (!driveIsFormatted) {
+      console.log(`Formatting drive with key ${encryptionKey}...`);
+      await execCommand(dedent`echo '${encryptionKey}' | sudo cryptsetup --batch-mode -d - luksFormat ${devicePath}`);
     }
-  } catch (error) {}
-
-  // Attempt to mount the virtual partition.
-  try {
-    if (driveIsEncrypted) {
-      execSync(dedent`
-        sudo mkdir -p ${mountPath};
-        echo '${encryptionKey}' | sudo cryptsetup --batch-mode -d - luksOpen ${devicePath} ${encryptionKey};
-        sudo mount /dev/mapper/${encryptionKey} ${mountPath}
-      `);
-    }
-  } catch (error) {}
+  }
 
   if (driveIsEncrypted) {
-    console.log(`${devicePath} is already encrypted!`);
+    // Attempt to mount the virtual partition.
+    await execCommand(dedent`
+      sudo mkdir -p ${mountPath};
+      echo '${encryptionKey}' | sudo cryptsetup --batch-mode -d - luksOpen ${devicePath} ${encryptionKey};
+      sudo mount /dev/mapper/${encryptionKey} ${mountPath}
+    `);
   } else {
-    // Setup and mount encrypted virtual partition.
-    try {
-      execSync(dedent`
-        echo '${encryptionKey}' | sudo cryptsetup --batch-mode -d - luksOpen ${devicePath} ${encryptionKey};
-      `);
-    } catch (error) {}
+    // Open and mount encrypted virtual partition.
+    await execCommand(
+      dedent`echo '${encryptionKey}' | sudo cryptsetup --batch-mode -d - luksOpen ${devicePath} ${encryptionKey}`
+    );
 
+    // Create filesystem on virtual partition.
     if (!driveIsFormatted) {
-      try {
-        execSync(dedent`
-          yes | sudo mkfs.ext4 -q /dev/mapper/${encryptionKey};
-        `);
-      } catch (error) {}
+      await execCommand(dedent`yes | sudo mkfs.ext4 -q /dev/mapper/${encryptionKey}`);
     }
 
-    try {
-      execSync(dedent`
-        sudo mkdir -p ${mountPath};
-        sudo mount /dev/mapper/${encryptionKey} ${mountPath};
-        sudo chmod 755 -R ${mountPath}
-      `);
-    } catch (error) {}
+    // Mount virtual partition.
+    await execCommand(dedent`
+      sudo mkdir -p ${mountPath};
+      sudo mount /dev/mapper/${encryptionKey} ${mountPath};
+      sudo chmod 755 -R ${mountPath}
+    `);
   }
+}
+
+function execCommand(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      resolve(stdout ? stdout : stderr);
+    });
+  });
 }
