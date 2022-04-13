b5ff5452eb6b04c72bddb73761292d1ef4d41cb1
Jack Swayze <talk2dug@gmail.com>
2022-04-13 01:05:19 -0500

    moved streaming to its own pm2 service

--- helperFunctions.js
+++ helperFunctions.js
@@ -41,33 +41,90 @@ const writeFile = (file, text) => {
 };
 
 const startMediaServer = async (config) => {
-  for (var i = 0; i < cameras.length; i++) {
-    console.log(`Spawning ffmpeg for Streaming ${cameras[i].folder}/${cameras[i].address}...`);
-    
-
-    child = spawn(
+ /* var child1
+  var child2
+  var child3
+  const formatArguments = (template) => {
+      return template
+        .replace(/\s+/g, ' ')
+        .replace(/\s/g, '\n')
+        .split('\n')
+        .filter((arg) => (arg != '' ? true : false));
+    };
+   child1 = spawn(
       '/home/pi/u/tool/ffserver/bin/ffmpeg',
       formatArguments(`
       -hide_banner 
       -loglevel error
         -rtsp_transport tcp
-        -i rtsp://${process.env.CAMERA_USER}:${process.env.CAMERA_PASSWORD}@${cameras[i].address}/cam/realmonitor?channel=1&subtype=1
+        -i rtsp://admin:UUnv9njxg123@10.10.5.2/cam/realmonitor?channel=1&subtype=1
         -r 15 
-        -an http://127.0.0.1:${cameras[i].serverPort}/${cameras[i].folder}.ffm
+        -an http://127.0.0.1:8090/camera1.ffm
         
        
       `)
     );
-
-    child.stderr.on('data', (data) => {
+    child1.on("close", function() {
+      // Wait for process to exit, then run again
+  });
+  
+    child1.stderr.on('data', (data) => {
       console.error(`${data}`);
+   
     });
-  }
+  
+  
+  
+  
+  
+    child2 = spawn(
+      '/home/pi/u/tool/ffserver/bin/ffmpeg',
+      formatArguments(`
+      -hide_banner 
+      -loglevel error
+        -rtsp_transport tcp
+        -i rtsp://admin:UUnv9njxg123@10.10.5.3/cam/realmonitor?channel=1&subtype=1
+        -r 15 
+        -an http://127.0.0.1:8091/camera2.ffm
+        
+       
+      `)
+    );
+    child2.on("close", function() {
+      // Wait for process to exit, then run again
+  });
+  
+    child2.stderr.on('data', (data) => {
+      console.error(`${data}`);
+   
+    });
+  
+    child3 = spawn(
+      '/home/pi/u/tool/ffserver/bin/ffmpeg',
+      formatArguments(`
+      -hide_banner 
+      -loglevel error
+        -rtsp_transport tcp
+        -i rtsp://admin:UUnv9njxg123@10.10.5.4/cam/realmonitor?channel=1&subtype=1
+        -r 15 
+        -an http://127.0.0.1:8092/camera3.ffm
+        
+       
+      `)
+    );
+    child3.on("close", function() {
+      // Wait for process to exit, then run again
+  });
+  
+    child3.stderr.on('data', (data) => {
+      console.error(`${data}`);
+   
+    });
 
 
 
 
-
+*/
 }
 
 
