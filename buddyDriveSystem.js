3880552b6a400a0adf677b2bc4b998f5ddf9a892
Jack Swayze <talk2dug@gmail.com>
2022-05-26 23:10:07 -0500

    working on auto SSH keys install

--- buddyDriveSystem.js
+++ buddyDriveSystem.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
-
+const got = require('got')
+const fetch = require('node-fetch');
+  const promise = require('promise');
 const { exec, execSync, spawn } = require('child_process');
 const formatArguments = (template) => {
     return template
@@ -16,12 +18,27 @@ const execCommand = (command) => {
       });
     });
   };
+let nodes = []
+  fetch('http://rtcc-server.shreveport-it.org:3000/api/nodes')
+  .then((response) => response.json())
+  .then((json) => {
+    json.forEach(
+      function (node) {
+        
+        try {
+          execCommand(`sshpass -p ${process.env.SSH_KEY} ssh-copy-id pi@`+node.config.ip);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    );
+ 
+  });
+   
+
+ 
+//Loop RSYNC every hour
 
 
-
-  execCommand(dedent`
-
-`);
-
-
+//rsync -avzh /home/pi/videos/camera1 pi@10.10.30.141:/home/pi/remote_backups/CrimeCamera074/caemra1/
 
