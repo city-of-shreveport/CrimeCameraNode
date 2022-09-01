7187b670642d4ca1b4397b6df144115f2ec10936
DR <drsvpla@gmail.com>
2022-09-01 15:15:57 -0700

    Add git commit to swStatus, and also set status to degraded if any pm2 module has had a restart

--- services/bootstrapServices/everyBoot/swStatus.js
+++ services/bootstrapServices/everyBoot/swStatus.js
@@ -14,9 +14,11 @@ async function run() {
 /*
   STATUS REPORTING
   healthy   -- Everything looks ok
+  degraded  -- any pm2 module has a higher number of restarts than "last time"
   emergency -- Any pm2 module is not 'online'
 */
 
+var last_restarts=[];
 async function getData() {
   var dat={
     status:"healthy",
@@ -26,11 +28,17 @@ async function getData() {
   }
 
   for(var i=0;i<dat.pm2.length;i++) {
-    if(dat.pm2[i].status!='online')
+    if(dat.pm2[i].status!='online') {
       dat.status='emergency'
+      break; // no need to look further
+    }
+    if(dat.pm2[i].restarts > last_restarts[i]) // the first time through the last_restarts will all be undefined, #>undef is false, so this wokrs
+      dat.status='degraded';
   }
+  last_restarts=dat.pm2.map(p=>p.restarts);
   return dat;
 }
+
 
 function processPM2Data(dat) {
   return {
@@ -69,12 +77,14 @@ async function getGitData() {
   try {
     var d=await execCommand(`git status --porcelain -b`)
     d=d.stdout.trim().split('\n')
+    var commit=(await execCommand(`git rev-parse HEAD`)).stdout.trim()
     var branch_line=d.shift();
     var branch=branch_line.match(/^## (.*?)\.\.\./);
     var ahead=branch_line.match(/ahead ([0-9]+)/);
     var behind=branch_line.match(/behind ([0-9]+)/);
     return {
       branch:branch?branch[1]:'<unknown>',
+      commit,
       ahead:ahead?parseInt(ahead[1]):0,
       behind:behind?parseInt(behind[1]):0,
       dirtyFiles:d.length
