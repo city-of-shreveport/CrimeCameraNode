47b6d0e3eb26c2960dd4035f0f410ed993158020
DR <drsvpla@gmail.com>
2022-09-06 20:34:03 -0700

    A few fixes to hud.sh

--- scripts/hud.sh
+++ scripts/hud.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/dash
 
-df -h
+pm2 status
 
 echo
+echo
+echo
 
+df -h | grep -v -E ' /dev|/run|/sys|/boot'
+
+echo
+echo
+echo
+
+echo "Mounts"
 mount | grep mapper
 
 echo
+echo
+echo
 
 dir=$(dirname $0)
-
 nvr=$(node $dir/summarizeNVRJSFiles.js 15)
 echo "$nvr" | head -1
-echo "$nvr" | tail -15
-
+echo "$nvr" | tail -5
 
